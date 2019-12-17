99d6e48cc28a9dacf3a9773a55a6ad221fa38439
Franklin Yang <fyang016@ucr.edu>
2019-12-17 14:40:44 -0600

    added playground file

---
+++ Fibonacci.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+//Building a Fibonacci number generator
+//Function to check if a number is prime or not
+//Function to calculate factorial of a number
+
+// var str = "Hello, playground"
+
+func isPrime(number : Int) -> Bool {
+    for i in 1...number {
+        if (number )
+    }
+}
+
+func factorial(number : Int) -> Int {
+    var placeholder = 1
+    var numCount = number
+    while (numCount > 1) {
+        placeholder = placeholder * numCount
+        numCount = numCount - 1
+    }
+    return placeholder
+}
+
+
+print(factorial(number : 6))
